4c31d6913c9f937ba1ef2dff61c61926c6c7bb6d
ParniyanMousaie-SternX <parniyan.mousaie@sternx.de>
2025-05-23 17:25:52 +0330

    Add version 1.1.2

--- kittylogger/build.gradle.kts
+++ kittylogger/build.gradle.kts
@@ -65,7 +65,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.parniyan.kittylogger"
                 artifactId = "kittylogger"
-                version = "1.1.0"
+                version = "1.1.2"
                 from(components.findByName("release"))
             }
         }
